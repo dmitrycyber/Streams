bb50c611180ce7bacbf668aff0e51f2661236d5a
Dmitry <dmitry.ivoninsky@gmail.com>
2019-12-24 01:06:39 +0300

    Solved recursion tasks

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        //recursion.printNumbersBefore(5);
+        //System.out.println(recursion.getSumOfNumberDigits(4444));
+        System.out.println(recursion.isPalindrome("qwe4wewq"));
+
+    }
+}
---
+++ src/main/java/Recursion.java
@@ -0,0 +1,28 @@
+public class Recursion {
+
+    public int printNumbersBefore(int number){
+        if (number == 0){
+            return 0;
+        }
+        System.out.println(number);
+        return printNumbersBefore(number - 1);
+    }
+
+    public int getSumOfNumberDigits(int number){
+        if (number < 10){
+            return number%10;
+        }
+        return number%10 + getSumOfNumberDigits(number/10);
+    }
+
+    public boolean isPalindrome(String text){
+        if (text.length() <= 1){
+            return true;
+        }
+        if (text.charAt(0) == text.charAt(text.length()-1)){
+            String s = text.substring(1, text.length()-1);
+            return isPalindrome(s);
+        }
+        return false;
+    }
+}
